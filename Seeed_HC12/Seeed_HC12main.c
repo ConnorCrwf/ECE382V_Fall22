b74842a4b51cce3d492fd79ff6fe094d28390ac8
unknown <dmeza@utexas.edu>
2022-09-07 17:43:40 -0500

    Code changes as of 090722. Main application and SysTick ISR are handled through trampoline functions. Section 5E was completed.

--- Seeed_HC12/Seeed_HC12main.c
+++ Seeed_HC12/Seeed_HC12main.c
@@ -67,6 +67,7 @@ policies, either expressed or implied, of the FreeBSD Project.
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "msp.h"
 #include "..\inc\CortexM.h"
 #include "..\inc\SysTickInts.h"
@@ -85,43 +86,6 @@ volatile uint32_t Time,MainCount;
 char HC12data;
 uint32_t Message;
 int Flag; // semaphore
-// every 10ms
-void SysTick_Handler(void){
-  uint8_t in;
-  LEDOUT ^= 0x01;       // toggle P1.0
-  LEDOUT ^= 0x01;       // toggle P1.0
-  Time = Time + 1;
-  uint8_t ThisInput = LaunchPad_Input();   // either button
-  if(ThisInput){
-    if((Time%100) == 0){ // 1 Hz
-      HC12data = HC12data^0x01; // toggle '0' to '1'
-      if(HC12data == 0x31){
-        Message = 1; // S1
-        Flag = 1;    // signal
-       }else{
-        Message = 0; // S0
-        Flag = 1;    // signal
-      }
-      UART1_OutChar(HC12data);
-    }
-  }
-  in = UART1_InCharNonBlock();
-  if(in){
-    switch(in){
-      case '0':
-        Message = 2; // R0
-        Flag = 1;    // signal
-        LaunchPad_Output(0); // off
-        break;
-      case '1':
-        Message = 3; // R1
-        Flag = 1;    // signal
-        LaunchPad_Output(BLUE);
-        break;
-    }
-  }
-  LEDOUT ^= 0x01;       // toggle P1.0
-}
 
 void HC12_ReadAllInput(void){uint8_t in;
 // flush receiver buffer
@@ -161,10 +125,48 @@ void HC12_Init(uint32_t baud){
   //************ configuration ended***********************
   printf("\nRF_XMT initialization done\n");
 }
+
+void SysTick_Handler_1(void){
+  uint8_t in;
+  LEDOUT ^= 0x01;       // toggle P1.0
+  LEDOUT ^= 0x01;       // toggle P1.0
+  Time = Time + 1;
+  uint8_t ThisInput = LaunchPad_Input();   // either button
+  if(ThisInput){
+    if((Time%100) == 0){ // 1 Hz
+      HC12data = HC12data^0x01; // toggle '0' to '1'
+      if(HC12data == 0x31){
+        Message = 1; // S1
+        Flag = 1;    // signal
+       }else{
+        Message = 0; // S0
+        Flag = 1;    // signal
+      }
+      UART1_OutChar(HC12data);
+    }
+  }
+  in = UART1_InCharNonBlock();
+  if(in){
+    switch(in){
+      case '0':
+        Message = 2; // R0
+        Flag = 1;    // signal
+        LaunchPad_Output(0); // off
+        break;
+      case '1':
+        Message = 3; // R1
+        Flag = 1;    // signal
+        LaunchPad_Output(BLUE);
+        break;
+    }
+  }
+  LEDOUT ^= 0x01;       // toggle P1.0
+}
+
 /**
  * main.c
  */
-void main(void){int num=0;
+void main_1(void){int num=0;
   Clock_Init48MHz();        // running on crystal
   Time = MainCount = 0;
   SysTick_Init(480000,2);   // set up SysTick for 100 Hz interrupts
@@ -216,3 +218,313 @@ void main(void){int num=0;
   }
 }
 
+
+// **************************************************
+uint8_t KnownSequence[] = {'A', 'B', 'C', 'D', 'E'};
+uint8_t InputSequence[5];
+
+//********SysTick_Handler*****************
+// ISR for SysTick timer.
+// Checks UART1 for communication through HC12 wireless module.
+// Sends data when button is pressed.
+// Inputs: none
+// Outputs: none
+void SysTick_Handler_5E(void) {
+  uint8_t i, j;
+  uint8_t in;
+
+  LEDOUT ^= 0x01;       // toggle P1.0
+  LEDOUT ^= 0x01;       // toggle P1.0
+  Time = Time + 1;
+
+  // Clear input message from previous run
+  InputSequence[0] = '\0';
+  InputSequence[1] = '\0';
+  InputSequence[2] = '\0';
+  InputSequence[3] = '\0';
+  InputSequence[4] = '\0';
+
+  // Send data on button press at a rate of 1 Hz
+  uint8_t ThisInput = LaunchPad_Input();   // either button
+  if (ThisInput) {
+    if ((Time % 100) == 0) {  // 1 Hz
+      Message = 1;
+      Flag = 1;    // signal
+      for (i = 0; i < sizeof(KnownSequence); i++) {
+        UART1_OutChar(KnownSequence[i]);
+      }
+    }
+  }
+
+  // Receive data and compare to known sequence
+  in = UART1_InCharNonBlock();
+  j = 0;
+  while (in) {
+    Message = 2;
+    Flag = 1;
+    InputSequence[j] = in;
+    in = UART1_InCharNonBlock();
+    j = (j + 1) % 5;
+  }
+  // Check for correct sequence received
+  if (Flag && Message == 2) {
+    if (memcmp(KnownSequence, InputSequence, 5) != 0)
+      Message = 3;
+      while (UART1_InCharNonBlock()) {}
+  }
+
+  LEDOUT ^= 0x01;       // toggle P1.0
+}
+
+//********Main_5E*****************
+// Entry point of the application for section 5E
+// Inputs: none
+// Outputs: none
+void main_5E(void) {
+  int num = -1;
+  DisableInterrupts();  // Prevent interrupts during initialization
+
+  // Standard initialization sequence
+  Clock_Init48MHz();        // running on crystal
+  SysTick_Init(480000,2);   // set up SysTick for 100 Hz interrupts
+  LaunchPad_Init();         // P1.0 is red LED on LaunchPad
+  UART0_Initprintf();       // serial port to PC for debugging
+  SSD1306_Init(SSD1306_SWITCHCAPVCC);
+
+  // Print initial message to display
+  SSD1306_OutClear();
+  SSD1306_SetCursor(0,0);
+  SSD1306_OutString("----- ECE382V ------\n");
+  SSD1306_OutString(" Lab 1\n");
+  SSD1306_OutString(" Section 5E\n");
+  EnableInterrupts();
+  printf("\nSection 5E\n");
+
+  // Initialize wireless module (may take some time)
+  HC12_Init(UART1_BAUD_9600);
+  SSD1306_OutString(" RF_XMT init done\n");
+  SSD1306_OutString("\nHold switch for 1s\n");
+
+  EnableInterrupts();
+
+  // Application, loop forever
+  while (1) {
+    WaitForInterrupt();
+    MainCount++;
+
+    if (Flag) {   // wait on semaphore
+      num++;
+      if (num == 1) {
+        SSD1306_OutClear(); // Clear display
+      }
+
+      Flag = 0;   // Data transmission has been handled
+      SSD1306_OutUDec16(num);
+      SSD1306_OutChar(' ');
+      switch (Message) {
+        case 1:   // Send sequence
+          printf("S\n");
+          SSD1306_OutString("S               \n");
+          break;
+        case 2:
+          printf("R\n");
+          SSD1306_OutString("R ");
+          SSD1306_OutChar(InputSequence[0]);
+          SSD1306_OutChar(InputSequence[1]);
+          SSD1306_OutChar(InputSequence[2]);
+          SSD1306_OutChar(InputSequence[3]);
+          SSD1306_OutChar(InputSequence[4]);
+          SSD1306_OutString("\n");
+          break;
+        case 3:
+          printf("L\n");
+          SSD1306_OutString("L ");
+          SSD1306_OutChar(InputSequence[0]);
+          SSD1306_OutChar(InputSequence[1]);
+          SSD1306_OutChar(InputSequence[2]);
+          SSD1306_OutChar(InputSequence[3]);
+          SSD1306_OutChar(InputSequence[4]);
+          SSD1306_OutString("\n");
+          break;
+      }
+    }
+  }
+}
+
+
+// **************************************************
+uint8_t deviceID = 0;
+uint8_t numDevices = 1;
+uint8_t SendFlag = 0;
+struct MessageProtocol{
+  uint8_t header;
+  uint8_t address;
+  uint8_t length;
+  uint8_t data[32];
+  uint8_t error;
+};
+struct MessageProtocol OutputPacket;
+struct MessageProtocol InputPacket;
+
+
+//********SysTick_Handler*****************
+// ISR for SysTick timer.
+// Checks UART1 for communication through HC12 wireless module.
+// Sends data when button is pressed.
+// Inputs: none
+// Outputs: none
+void SysTick_Handler_5F(void) {
+  uint8_t in;
+
+  LEDOUT ^= 0x01;       // toggle P1.0
+  LEDOUT ^= 0x01;       // toggle P1.0
+  Time = Time + 1;
+
+  // Check for system initialization complete
+  if (numDevices >= 3) {
+    // Send data on button press
+    //TODO Check if this works or needs denouncing (counter ~30) before proceeding.
+    uint8_t ThisInput = LaunchPad_Input();   // either button
+    if (ThisInput) {
+      if (!SendFlag) {    // Send a single message
+        SendFlag = 1;
+        // SendPacket(OutputPacket);
+      }
+    }
+    else {
+      SendFlag = 0;   // Reset flag when button is released
+    }
+
+    // Receive data and perform error checking
+    in = UART1_InCharNonBlock();
+    if (in) {
+      // ReceivePacket(in);
+    }
+  }
+}
+
+//********Main_5F*****************
+// Entry point of the application for section 5F
+// Inputs: none
+// Outputs: none
+void main_5F(void) {
+  uint8_t i = 1, error;
+
+  DisableInterrupts();  // Prevent interrupts during initialization
+
+  // Standard initialization sequence
+  Clock_Init48MHz();        // running on crystal
+  SysTick_Init(480000,2);   // set up SysTick for 100 Hz interrupts
+  LaunchPad_Init();         // P1.0 is red LED on LaunchPad
+  UART0_Initprintf();       // serial port to PC for debugging
+  SSD1306_Init(SSD1306_SWITCHCAPVCC);
+
+  // Print initial message to display
+  SSD1306_OutClear();
+  SSD1306_SetCursor(0,0);
+  SSD1306_OutString("----- ECE382V ------\n");
+  SSD1306_OutString(" Lab 1\n");
+  SSD1306_OutString(" Section 5F\n");
+  EnableInterrupts();
+  printf("\nSection 5F\n");
+
+  // Initialize wireless module (may take some time)
+  HC12_Init(UART1_BAUD_9600);
+  SSD1306_OutString(" RF_XMT init done\n");
+  SSD1306_OutString("\nHold switch for 1s\n");
+
+  // Create output message
+  OutputPacket.header = '?';    // Unlikely any other team chose this symbol
+  OutputPacket.address = 2;
+  OutputPacket.length = 3;
+  OutputPacket.data[0] = 'A';
+  OutputPacket.data[1] = 'B';
+  OutputPacket.data[2] = 'C';
+  i = 1;
+  error = OutputPacket.data[0];
+  while (i < OutputPacket.length) {
+    error = error ^ OutputPacket.data[i];
+    i++;
+  }
+  OutputPacket.error = error;   // Bitwise XOR of data sequence
+
+  EnableInterrupts();
+
+  // Application, loop forever
+  while (1) {
+    //TODO Make sure initialization sequence makes sense.
+    // Initialization step until three devices are online.
+    if (numDevices < 3) {
+      // Check for device ID
+      if (deviceID) {
+        LaunchPad_Output(GREEN);    // LED Green
+        i = UART1_InCharNonBlock();
+        // Check for new device online
+        if (i == 0) {
+          numDevices++;
+          UART1_OutChar(numDevices);
+        }
+      }
+      else {
+        LaunchPad_Output(0);        // LED Off
+        UART1_OutChar(deviceID);    // Sends 0
+        i = UART1_InChar();
+        if (i) {    // Received nonzero, previous device(s) online
+          deviceID = i;
+          numDevices = i;
+        }
+        else {      // Received 0, I'm the first device
+          deviceID = 1;
+          numDevices++;
+          UART1_OutChar(numDevices);
+        }
+      }
+    }
+
+    // Three devices are online, continue with message transmission.
+    else {
+      
+    }
+  }
+}
+
+
+int trampoline = 2;
+
+//********SysTick_Handler*****************
+// ISR for SysTick timer, runs every 10ms.
+// Trampoline for selecting handler.
+// Inputs: none
+// Outputs: none
+void SysTick_Handler(void) {
+  switch (trampoline) {
+    case 0:
+      SysTick_Handler_1();
+      break;
+    case 1:
+      SysTick_Handler_5E();
+      break;
+    case 2:
+      SysTick_Handler_5F();
+      break;
+  }
+}
+
+//********Trampoline for selecting main*****************
+void main() {
+  switch (trampoline) {
+    case 0:
+      main_1();
+      break;
+    case 1:
+      main_5E();
+      break;
+    case 2:
+      main_5F();
+      break;
+  }
+}
+
+
+//TODO *** Add profiling for latency measurement.
+// Look at Pete's Code to implement the message protocol.
